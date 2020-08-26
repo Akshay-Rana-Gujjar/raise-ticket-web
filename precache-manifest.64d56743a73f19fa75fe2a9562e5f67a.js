3e20676fdbcbc491842216f9e047b2b56f3d7cf8
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2020-08-26 16:05:28 +0530

    add success page, change domain to custom domain

---
+++ precache-manifest.64d56743a73f19fa75fe2a9562e5f67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52222599071dfd82e69452fc25834533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0908de7a675225b17d24",
+    "url": "/static/css/main.25eee723.chunk.css"
+  },
+  {
+    "revision": "d4f9b601c2bdfddfa4f0",
+    "url": "/static/js/2.4ce4c957.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.4ce4c957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0908de7a675225b17d24",
+    "url": "/static/js/main.8cfdc375.chunk.js"
+  },
+  {
+    "revision": "c4da3c9b534d22e4614d",
+    "url": "/static/js/runtime-main.03cdb43b.js"
+  }
+]);
